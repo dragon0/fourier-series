340e9ac00e0ff7d7e7d79e3d85666acc287736b6
Raven <sravendragon0@hotmail.com>
2019-10-16 18:25:39 -0700

    Add multiple functions

--- fourier.py
+++ fourier.py
@@ -5,6 +5,20 @@ import pygame
 
 WHITE = (255,255,255)
 
+def square(i, theta):
+    n = i * 2 + 1
+    radius = 50 * (4/(n*pi))
+    x = radius * cos(n * theta)
+    y = radius * sin(n * theta)
+    return x, y, radius
+
+def sawtooth(i, theta):
+    n = i + 1
+    radius = 50 * (2/(n*pi * (-1 if n%2 == 0 else 1)))
+    x = radius * cos(n * theta)
+    y = radius * sin(n * theta)
+    return x, y, abs(radius)
+
 class Game:
     def __init__(self):
         pass
@@ -25,8 +39,11 @@ class Game:
         self._time = 0
         self._steps = 1
         self._wave = []
+        self._func = square
         self._up_button = pygame.Rect(50, 50, 30, 30)
         self._dn_button = pygame.Rect(50, 100, 30, 30)
+        self._square_button = pygame.Rect(150, 50, 30, 30)
+        self._saw_button = pygame.Rect(150, 100, 30, 30)
 
     def _shutdown(self):
         pass
@@ -54,9 +71,13 @@ class Game:
             if self._dn_button.collidepoint(self._bpos):
                 if self._steps > 1:
                     self._steps -= 1
+            if self._square_button.collidepoint(self._bpos):
+                self._func = square
+            if self._saw_button.collidepoint(self._bpos):
+                self._func = sawtooth
 
         dx = 150
-        dy = 200
+        dy = 250
         radius = 50
 
         x = 0
@@ -66,10 +87,10 @@ class Game:
         for i in range(self._steps):
             prevx = x
             prevy = y
-            n = i * 2 + 1
-            radius = 50 * (4/(n*pi))
-            x += radius * cos(n * self._time)
-            y += radius * sin(n * self._time)
+
+            nx, ny, radius = self._func(i, self._time)
+            x += nx
+            y += ny
 
             el = pygame.Rect(0, 0, radius*2, radius*2)
             el.centerx = dx+prevx
@@ -113,8 +134,8 @@ class Game:
 
         pygame.draw.rect(self._screen, WHITE, self._dn_button, 1)
         pygame.draw.line(self._screen, WHITE,
-                (self._up_button.centerx+10, self._dn_button.centery),
-                (self._up_button.centerx-10, self._dn_button.centery))
+                (self._dn_button.centerx+10, self._dn_button.centery),
+                (self._dn_button.centerx-10, self._dn_button.centery))
 
         x = 90
         y = 50
@@ -124,6 +145,21 @@ class Game:
             if x >= 140:
                 x = 90
                 y += 5
+
+        pygame.draw.rect(self._screen, WHITE, self._square_button, 1)
+        pygame.draw.lines(self._screen, WHITE, False,[
+                (self._square_button.centerx+10, self._square_button.centery+10),
+                (self._square_button.centerx+10, self._square_button.centery-10),
+                (self._square_button.centerx-10, self._square_button.centery-10),
+                (self._square_button.centerx-10, self._square_button.centery+10)
+            ])
+
+        pygame.draw.rect(self._screen, WHITE, self._saw_button, 1)
+        pygame.draw.lines(self._screen, WHITE, False,[
+                (self._saw_button.centerx+10, self._saw_button.centery+10),
+                (self._saw_button.centerx+10, self._saw_button.centery-10),
+                (self._saw_button.centerx-10, self._saw_button.centery+10)
+            ])
 
         pygame.display.flip()
 
